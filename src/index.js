56f71aaf6f7129b001f6cf9d0d24def876faf44a
petercrawford <pcraw1027@gmail.com>
2021-03-24 11:09:22 -0400

    push to Danny

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+let url = "http://localhost:3000/toys"
+let toyCollection = document.querySelector("div#toy-collection")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+fetch(url)
+.then(res => res.json())
+.then(data => data.forEach(console.log(data.name))) 
+
+function addToys() {
+
+let div = document.createElement("div")
+div.className = "card" 
+let h2 = document.createElement("h2")
+h2.innerText = "...data.name"
+let img = document.createElement("img")
+img.src = "...data.image"
+img.className = "toy-avatar"
+let p = document.createElement("p")
+p.innerText = "...data.likes"
+let createButton = document.createElement("button")
+createButton.className = "like-btn"
+createButton.innerText = "Like"
+
+div.append(h2, img, p, createButton)
+
+toyCollection.append(div)
+
+
+}
+
+addToys()
+
+
